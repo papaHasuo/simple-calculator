4945fe46db25bd91e7072f8c21a288bbf257d4e9
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-06 18:12:21 +0900

    足した結果を返すように変更

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("simple-calculator")
 
 @mcp.tool()
-async def add(a: int, b: int) -> str:
+async def add(a: int, b: int) -> int:
     """二つの引数を足して返す関数
 
     Args:
         a: 足す数
         b: 足される数
     """
-    return "\n---\n".join([str(a), str(b), str(a + b)])
+    return a+b
 
 if __name__ == "__main__":
     # Initialize and run the server
