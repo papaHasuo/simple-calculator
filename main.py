56bbae0f474ed9750e44fd861830ffbe1f3e7209
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-06 18:13:56 +0900

    それ以外の四則演算についても追加

--- main.py
+++ main.py
@@ -10,10 +10,42 @@ async def add(a: int, b: int) -> int:
     """二つの引数を足して返す関数
 
     Args:
-        a: 足す数
-        b: 足される数
+        a: 足される数
+        b: 足す数
     """
-    return a+b
+    return a + b
+
+@mcp.tool()
+async def minus(a: int, b: int) -> int:
+    """二つの引数を引いて返す関数
+
+    Args:
+        a: 引かれる数
+        b: 引く数
+    """
+    return a - b
+
+@mcp.tool()
+async def multiply(a: int, b: int) -> int:
+    """二つの引数を掛けて返す関数
+
+    Args:
+        a: 掛けられる数
+        b: 掛ける数
+    """
+    return a * b
+
+@mcp.tool()
+async def divide(a: int, b: int) -> float:
+    """二つの引数を割って返す関数
+
+    Args:
+        a: 割られる数
+        b: 割る数
+    """
+    if b == 0:
+        raise ValueError("Division by zero is not allowed.")
+    return a / b
 
 if __name__ == "__main__":
     # Initialize and run the server
