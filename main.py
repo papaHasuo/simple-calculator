b0587812bd10a8771f4e5339b9b0de6d7c7f7919
papa_hasuo <papa_hasuo@outlook.jp>
2025-07-06 18:04:34 +0900

    単純な足し算機能を持つサーバを作成

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from simple-calculator!")
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
 
+# Initialize FastMCP server
+mcp = FastMCP("simple-calculator")
+
+@mcp.tool()
+async def add(a: int, b: int) -> str:
+    """二つの引数を足して返す関数
+
+    Args:
+        a: 足す数
+        b: 足される数
+    """
+    return "\n---\n".join([str(a), str(b), str(a + b)])
 
 if __name__ == "__main__":
-    main()
+    # Initialize and run the server
+    mcp.run(transport='stdio')
